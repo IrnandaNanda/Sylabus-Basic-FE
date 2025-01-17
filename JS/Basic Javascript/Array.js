ad6c7465e1e3d56c2f8e8ae1ab07cc6efaff9ad0
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-17 21:51:47 +0700

    Array

---
+++ JS/Basic Javascript/Array.js
@@ -0,0 +1,23 @@
+let names = ["uli", "farhan", "ahmad"]
+
+console.table(names)
+
+let fruits = ["Apple", "Banana", "Orange"]
+console.table(fruits);
+
+
+// Untuk menambahkan array
+fruits.push("Watermelon");
+console.table(fruits);
+
+
+// Untuk menghapus array
+fruits.pop()
+console.table(fruits);
+
+
+// Untuk mengganti Array berdasarkan Index
+fruits[1] = "Lemon";
+console.table(fruits);
+
+
